8ce72197eeae9f61d74bab75a0b812a7f9c2e27d
Muhammad Taufik Hidayat <44204036+jinggatra@users.noreply.github.com>
2024-07-23 09:30:34 +0700

    add model post

---
+++ application/models/M_post.php
@@ -0,0 +1,36 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class M_post extends CI_Model{
+    public function __construct() {
+        $this->load->database();
+    }
+
+    public function get_post(){
+        $this->db->select('post.*, account.name');
+        $this->db->from('post');
+        $this->db->join('account','post.username = account.username');
+        $query = $this->db->get();
+        return $query->result_array();
+    }
+
+    public function get_post_by_id($id) {
+        $query = $this->db->get_where('post', array('idpost' => $id));
+        return $query->row_array();
+    }
+
+    public function create_post($data) {
+        return $this->db->insert('post', $data);
+    }
+
+    public function update_post($id, $data) {
+        $this->db->where('idpost', $id);
+        return $this->db->update('post', $data);
+    }
+
+    public function delete_post($id) {
+        $this->db->where('idpost', $id);
+        return $this->db->delete('post');
+    }
+
+}
+?>
